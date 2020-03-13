0b46d9f99d3293f6b48624f4042705b5fd934d17
HenryKasai <henrykasai1775@gmail.com>
2020-03-13 20:15:37 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá usuário! Escreva algo e veja como o Cebolinha diria isto.");
+
+            string cebola = Console.ReadLine();
+
+            Console.WriteLine(cebola .Replace("r" , "l"));
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Cebolinha
 Escreva como o Cebolinha
+
+
+Exercício:
+
+Solicite que o usuário digite uma frase. Exiba a frase como se ela fosse dita pelo Cebolinha, da Turma da Mônica.
+
+
+
+Lembre-se que o Cebolinha tem um disturbio na fala chamado dislalia, que o faz trocar o r pelo l.
